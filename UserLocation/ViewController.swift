b9976a74fb2104a90e7816c3817f6dd6cafbc33d
Mac <sudiptoroy@sudipto.local>
2020-02-06 15:55:12 +0600

    Initial Commit

---
+++ UserLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by Sudipto Roy on 2/6/20.
+//  Copyright © 2020 Code-X Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
