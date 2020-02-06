1a440403eceae8dbb2824717099642f070fa78ba
Mac <sudiptoroy@sudipto.local>
2020-02-06 16:16:21 +0600

    Add Basic Setup

--- UserLocation/ViewController.swift
+++ UserLocation/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManger = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // mapView.delegate = self as? MKMapViewDelegate
+    }
+    
+    func setupLocationManager () {
+        // setting up location manager delegate
+        locationManger.delegate = self
+    }
+    
+    func checkLocationServices () {
+        if CLLocationManager.locationServicesEnabled() {
+            // Setup location manager
+        } else {
+            // Show alert to letting the user know they have to turn on the location
+        }
     }
 
 
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Some code
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Some code
+    }
 }
 
